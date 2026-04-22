6be16fc36340b645022dcedfb32d0091c11273ea
FinnL <c24358871@mytudublin.ie>
2026-04-22 23:34:38 +0100

    Edited Member to inherit from Golfer and removed balance field

--- Member.cpp
+++ Member.cpp
@@ -1,39 +1,13 @@
 #include "Member.h"
 
-Member::Member()
+Member::Member() : Golfer()
 {
-    name = "Unknown";
-    handicap = 0;
     memberID = "M000";
-    balance = 0.0;
 }
 
-Member::Member(string n, int h, string id)
+Member::Member(string name, int handicap, string id) : Golfer(name, handicap)
 {
-    name = n;
-    handicap = h;
     memberID = id;
-    balance = 0.0;
-}
-
-void Member::setName(string n)
-{
-    name = n;
-}
-
-string Member::getName()
-{
-    return name;
-}
-
-void Member::setHandicap(int h)
-{
-    handicap = h;
-}
-
-int Member::getHandicap()
-{
-    return handicap;
 }
 
 void Member::setMemberID(string id)
@@ -46,20 +20,9 @@ string Member::getMemberID()
     return memberID;
 }
 
-void Member::setBalance(double b)
-{
-    balance = b;
-}
-
-double Member::getBalance()
-{
-    return balance;
-}
-
 void Member::display()
 {
-    cout << "Member: " << name
-         << " | Handicap: " << handicap
-         << " | ID: " << memberID
-         << " | Balance: " << balance << endl;
+    cout << "Member: " << getName()
+         << " | Handicap: " << getHandicap()
+         << " | ID: " << memberID << endl;
 }
